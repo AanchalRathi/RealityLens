15720590b48072c51834fa17ce54a56ea51a114b
Aanchal Rathi <aanchalrathiwork@gmail.com>
2026-03-30 23:56:40 +0530

    Delete eye_module (1).py

--- eye_module (1).py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def euclidean(p1, p2):
-    return np.linalg.norm(np.array(p1) - np.array(p2))
-
-def get_eye_aspect_ratio(landmarks):
-    # simplified fake EAR (placeholder for demo)
-    return np.random.uniform(0.2, 0.35)
